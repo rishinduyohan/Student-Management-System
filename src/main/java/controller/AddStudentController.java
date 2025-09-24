43f4d142a9f97813b09a393a8a2ddc7a20c61d30
rishinduyohan <rishinduyohan@gmail.com>
2025-09-24 16:04:23 +0530

    Create student add form and controller

--- src/main/java/controller/AddStudentController.java
+++ src/main/java/controller/AddStudentController.java
@@ -1,4 +1,27 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+
 public class AddStudentController {
+
+    public TextField txtBatchNo;
+    public TextField txtNic;
+    public TextField txtName;
+    public RadioButton radioOnline;
+    public RadioButton radioPhysical;
+
+    public void btnAddOnAction(ActionEvent actionEvent) {
+        System.out.println(txtBatchNo.getText()+" "+txtNic.getText()+" "+txtName.getText());
+        clearText();
+    }
+
+    public void btnCancelOnAction(ActionEvent actionEvent) {
+    }
+    private void clearText(){
+        txtBatchNo.setText("");
+        txtNic.setText("");
+        txtName.setText("");
+    }
 }
