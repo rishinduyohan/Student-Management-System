30e9f13d98847fe4ced924f49942ca33b71a30c0
rishinduyohan <rishinduyohan@gmail.com>
2025-09-24 15:06:26 +0530

    Create Login Controller and fxml form

--- src/main/java/controller/LoginFormController.java
+++ src/main/java/controller/LoginFormController.java
@@ -1,4 +1,31 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LoginFormController {
+    public TextField txtUsername;
+    public PasswordField txtPassword;
+    Stage stage = new Stage();
+
+    public void btnLoginOnAction(ActionEvent actionEvent) {
+        txtUsername.setText("");
+        txtPassword.setText("");
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource(""))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        stage.show();
+    }
+
+    public void btnCancelOnAction(ActionEvent actionEvent) {
+        System.exit(0);
+    }
 }
