4399c71bf09a40228a396c1aaa75b93fd4535be1
rishinduyohan <rishinduyohan@gmail.com>
2025-09-26 18:24:13 +0530

    Update student add form

--- src/main/java/controller/AddStudentController.java
+++ src/main/java/controller/AddStudentController.java
@@ -10,16 +10,15 @@ public class AddStudentController {
     public TextField txtBatchNo;
     public TextField txtNic;
     public TextField txtName;
-    public RadioButton radioOnline;
-    public RadioButton radioPhysical;
-    public Group radioGroup;
+    public Group CheckLecture;
 
     public void btnAddOnAction(ActionEvent actionEvent) {
-        System.out.println(txtBatchNo.getText()+" "+txtNic.getText()+" "+txtName.getText()+" "+radioGroup.toString());
+        System.out.println(txtBatchNo.getText()+" "+txtNic.getText()+" "+txtName.getText());
         clearText();
     }
 
     public void btnCancelOnAction(ActionEvent actionEvent) {
+        System.exit(0);
     }
     private void clearText(){
         txtBatchNo.setText("");
